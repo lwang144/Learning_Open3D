314ce7ae4880ccf18e0f448bd6881f02e1dc8ca8
liangyu <liangyu@student.chalmers.se>
2021-01-07 23:27:38 +0100

    python code

--- 5_SurfaceReconstruction/SurfaceReconstruction.py
+++ 5_SurfaceReconstruction/SurfaceReconstruction.py
@@ -7,11 +7,54 @@ import sys
 
 import open3d_tutorial as o3dtut
 
-mesh = o3d.io.read_triangle_mesh("../test_data/Bunny.ply")
-pcd = mesh.sample_points_poisson_disk(750)
-o3d.visualization.draw_geometries([pcd])
-alpha = 0.03
-print(f"alpha={alpha:.3f}")
-mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
-mesh.compute_vertex_normals()
-o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+pcd = o3dtut.get_eagle_pcd()
+print(pcd)
+o3d.visualization.draw_geometries([pcd],
+                                  zoom=0.664,
+                                  front=[-0.4761, -0.4698, -0.7434],
+                                  lookat=[1.8900, 3.2596, 0.9284],
+                                  up=[0.2304, -0.8825, 0.4101])
+
+print('run Poisson surface reconstruction')
+# Poisson surface reconstruction will also create triangles in areas of low 
+# point density, and even extrapolates into some areas
+with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+        pcd, depth=9)
+# densities return value that indicates for each vertex the density
+# A low density value means that the vertex is only supported by a 
+# low number of points from the input point cloud.
+print(mesh)
+o3d.visualization.draw_geometries([mesh],
+                                  zoom=0.664,
+                                  front=[-0.4761, -0.4698, -0.7434],
+                                  lookat=[1.8900, 3.2596, 0.9284],
+                                  up=[0.2304, -0.8825, 0.4101])
+
+print('visualize densities')
+# Violet indicates low density and yellow indicates a high density.
+densities = np.asarray(densities)
+density_colors = plt.get_cmap('plasma')(
+    (densities - densities.min()) / (densities.max() - densities.min()))
+density_colors = density_colors[:, :3]
+density_mesh = o3d.geometry.TriangleMesh()
+density_mesh.vertices = mesh.vertices
+density_mesh.triangles = mesh.triangles
+density_mesh.triangle_normals = mesh.triangle_normals
+density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+o3d.visualization.draw_geometries([density_mesh],
+                                  zoom=0.664,
+                                  front=[-0.4761, -0.4698, -0.7434],
+                                  lookat=[1.8900, 3.2596, 0.9284],
+                                  up=[0.2304, -0.8825, 0.4101])
+
+# Use the density values to remove vertices and triangles that have a low support.
+print('remove low density vertices')
+vertices_to_remove = densities < np.quantile(densities, 0.01)
+mesh.remove_vertices_by_mask(vertices_to_remove)
+print(mesh)
+o3d.visualization.draw_geometries([mesh],
+                                  zoom=0.664,
+                                  front=[-0.4761, -0.4698, -0.7434],
+                                  lookat=[1.8900, 3.2596, 0.9284],
+                                  up=[0.2304, -0.8825, 0.4101])
