5b1e0bc8a81fb7441ea6605607dc20f175155944
liangyu <liangyu@student.chalmers.se>
2021-01-21 22:20:52 +0100

    colored point cloud registration

---
+++ 10_ColoredPointCloudRegistration/README.md
@@ -0,0 +1,22 @@
+# Colored Point Cloud Registration
+The examples included above are based on the tutorial from Open3D using both Python and C++. Hope this can help you! 
+
+[**C++ Examples**]()
+
+[**Python Examples**]()
+
+**RUN C++ Example:** 
+```
+  mkdir build
+  cd build
+  cmake ..
+  make
+  cd ../bin
+
+```
+
+## Common Function Summary:
+  - 
+  ```
+  ```
+  
---
+++ 10_ColoredPointCloudRegistration/src/ColoredPointCloudRegistration.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <random>
+#include <cstdlib>
+
+#include <Eigen/Dense>
+
+#include "open3d/Open3D.h"
+
+void draw_registration_result(const open3d::geometry::PointCloud &source,
+                              const open3d::geometry::PointCloud &target,
+                              const Eigen::Matrix4d &Transforamtion){
+    std::shared_ptr<open3d::geometry::PointCloud> source_temp(new open3d::geometry::PointCloud);
+    std::shared_ptr<open3d::geometry::PointCloud> target_temp(new open3d::geometry::PointCloud);
+    *source_temp = source;
+    *target_temp = target;
+    //source_temp -> PaintUniformColor(Eigen::Vector3d(1, 0.706, 0));
+    //target_temp -> PaintUniformColor(Eigen::Vector3d(0, 0.651, 0.929));
+    source_temp -> Transform(Transforamtion);
+    open3d::visualization::DrawGeometries({source_temp, target_temp},
+                                  "Registration result");
+}
+
+int main(int argc, char* argv[]) 
+{
+    auto source = std::make_shared<open3d::geometry::PointCloud>();
+    auto target = std::make_shared<open3d::geometry::PointCloud>();
+    source = open3d::io::CreatePointCloudFromFile("../../test_data/ColoredICP/frag_115.ply");
+    target = open3d::io::CreatePointCloudFromFile("../../test_data/ColoredICP/frag_116.ply");
+
+    std::cout << "Load two point clouds and show initial pose." << std::endl;
+    //Set the initial transformation matrix
+    auto trans_init = Eigen::MatrixXd::Identity(4,4);
+    draw_registration_result(*source, *target, trans_init);
+
+    std::cout << "--------------------------------------------------" << std::endl; 
+    std::cout << "Point-to-plane ICP registration, threshold=0.02" << std::endl;
+    auto result_ICP = open3d::pipelines::registration::RegistrationICP(*source, *target, 0.02,
+                trans_init, open3d::pipelines::registration::TransformationEstimationPointToPoint());
+    auto evaluation = open3d::pipelines::registration::EvaluateRegistration(*source, *target, 0.02, result_ICP.transformation_);
+    std::cout << "  fitness: " << evaluation.fitness_  << " (The higher, the better)"<< std::endl;
+    std::cout << "  inlier_rms: " << evaluation.inlier_rmse_  << " (The lower, the better)"<< std::endl;
+    std::cout << "[Geometric constraint does not prevent two planar surfaces from slipping.]" << std::endl;
+    draw_registration_result(*source, *target, result_ICP.transformation_);
+    
+    std::cout << "--------------------------------------------------" << std::endl; 
+    std::cout << "[3] Colored pointcloud registration" << std::endl;
+    double voxel_radius[3] = {0.04, 0.02, 0.01};
+    int max_iter[3] = {50, 30, 14};
+    for(int i=0; i < 3; i++){
+        int iter = max_iter[i];
+        double radius = voxel_radius[i];
+        std::cout << "[" << iter << ", " << radius << ", " << i << "]" << std::endl;
+
+        std::cout << "[3-1]. Downsample with a voxel size: " << radius << std::endl;
+        auto source_down = source -> VoxelDownSample(radius);
+        auto target_down = target -> VoxelDownSample(radius);
+
+        std::cout << "[3-2]. Estimate normal. " << radius << std::endl;
+        source_down -> EstimateNormals();
+    }
+    
+    
+    
+    return 0;
+}
