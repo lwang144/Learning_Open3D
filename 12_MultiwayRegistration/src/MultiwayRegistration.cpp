3a6041c0142bdcffbddafe00a3e7285d331d351f
liangyu <liangyu@student.chalmers.se>
2021-02-01 22:07:48 +0100

    Multiway Registration Done

--- 12_MultiwayRegistration/src/MultiwayRegistration.cpp
+++ 12_MultiwayRegistration/src/MultiwayRegistration.cpp
@@ -13,7 +13,6 @@
 // Load 3 point clouds form folder 'test_data/ICP' 
 auto load_point_cloud(const double &voxel_size = 0.0){
     std::vector<std::shared_ptr<open3d::geometry::PointCloud>> PCDs;
-    std::cout << '\n' << "[ 1. Load Pointcloud: ]" << std::endl;
     for(uint8_t i = 0; i < 3; i ++){
         std::string filePath = "../../test_data/ICP/cloud_bin_" + std::to_string(i) + ".pcd";
         std::shared_ptr<open3d::geometry::PointCloud> pcd;
@@ -55,9 +54,9 @@ auto full_registration( const std::vector<std::shared_ptr<open3d::geometry::Poin
             std::tie(transformationICP, informationICP) = pairwise_registration(*PCDs[sourceID], *PCDs[targetID], 
                                                             max_correspondence_distance_coarse, max_correspondence_distance_fine);
             open3d::utility::LogInfo("   Build open3d::pipelines::registration::PoseGraph");
-            if(targetID == sourceID + 1){
-                open3d::utility::LogInfo("   PCD[{:d}, {:d}], --Odomentry Edge", sourceID, targetID);
-                auto odomentry_temp = transformationICP.cwiseProduct(odomentry); // Dot product
+            if(targetID == sourceID + 1){ // Odometry Case
+                open3d::utility::LogInfo("   PCD[{:d}, {:d}], --Odometry Edge", sourceID, targetID);
+                auto odomentry_temp = transformationICP * odomentry;
                 pose_graph.nodes_.push_back(open3d::pipelines::registration::PoseGraphNode(odomentry_temp.inverse()));
                 pose_graph.edges_.push_back(open3d::pipelines::registration::PoseGraphEdge(sourceID, targetID, 
                                                                     transformationICP, informationICP, false));
@@ -73,27 +72,50 @@ auto full_registration( const std::vector<std::shared_ptr<open3d::geometry::Poin
 }
 
 int main(){
-    //----- Prepare Point cloud -----//
+    open3d::visualization::Visualizer visualizer; //Create a visualizer object
+    //----- 1. Prepare Point cloud -----//
     double voxel_size = 0.02;
-    auto PCD_down = load_point_cloud(voxel_size);
+    std::cout << '\n' << "[ 1. Load Pointcloud: ]" << std::endl;
+    auto PCDs_down = load_point_cloud(voxel_size);
 
-    //----- Full Registration -----//
+    //----- 2. Full Registration -----//
     std::cout << '\n' << "[ 2. Full Registration: ]" << std::endl;
     double max_correspondence_distance_coarse = voxel_size * 15;
     double max_correspondence_distance_fine = voxel_size * 1.5;
-    auto pose_graph = full_registration(PCD_down, max_correspondence_distance_coarse, max_correspondence_distance_fine);
+    auto pose_graph = full_registration(PCDs_down, max_correspondence_distance_coarse, max_correspondence_distance_fine);
 
-    //----- Optimizing PoseGraph -----//
-    std::cout << '\n' << "[ 3. Optimizing PoseGraph: ]" << std::endl;
-    auto option =open3d::pipelines::registration::GlobalOptimizationOption(max_correspondence_distance_fine, 0.25, 0);
+    //----- 3. Optimizing PoseGraph -----//
+    std::cout << '\n' << "[ 3. Optimizing PoseGraph ...]" << std::endl;
+    auto option = open3d::pipelines::registration::GlobalOptimizationOption(max_correspondence_distance_fine, 0.25, 0);
     open3d::pipelines::registration::GlobalOptimization(pose_graph, open3d::pipelines::registration::GlobalOptimizationLevenbergMarquardt(),
                                                         open3d::pipelines::registration::GlobalOptimizationConvergenceCriteria(), option);
 
-    //----- Transform points and display -----//
+    //----- 4. Transform points and display -----//
     std::cout << '\n' << "[ 4. Transform points and display: ]" << std::endl;
-    for(int pointID = 0; pointID < PCD_down.size(); pointID ++){
-        open3d::utility::LogInfo("PCD_{:d}", pointID);
-
+    for(int pointID = 0; pointID < PCDs_down.size(); pointID ++){
+        open3d::utility::LogInfo("PCD_{:d}.nodes.pose:", pointID);
+        std::cout << "[" << pose_graph.nodes_[pointID].pose_ << "]" << std::endl;
+        PCDs_down[pointID] -> Transform(pose_graph.nodes_[pointID].pose_);
     }
+
+     //----- Point cloud Visualization -----//
+    visualizer.CreateVisualizerWindow("Transformed PCD", 1600, 900);
+    for(int ID = 0; ID < PCDs_down.size(); ID ++){    
+        visualizer.AddGeometry(PCDs_down[ID]);
+    }
+    visualizer.Run();
+    visualizer.DestroyVisualizerWindow();
+
+    //----- 5. Combine transformed PointClouds -----//
+    std::cout << '\n' << "[ 5. Combined transform Pointcloud: ]" << std::endl;
+    auto PCDs = load_point_cloud(voxel_size);
+    std::shared_ptr<open3d::geometry::PointCloud> PCDs_combined(new open3d::geometry::PointCloud);
+    for(int ID = 0; ID < PCDs.size(); ID ++){    
+        PCDs[ID] -> Transform(pose_graph.nodes_[ID].pose_);
+        *PCDs_combined += *PCDs[ID];
+    }
+    auto PCDs_combined_down = PCDs_combined -> VoxelDownSample(voxel_size);
+    open3d::io::WritePointCloud("./multiway_registration.pcd", *PCDs_combined_down);
+    open3d::visualization::DrawGeometries({PCDs_combined_down}, "Combined Multiway_registration PCD", 1600, 900);
     return 0;
 }
